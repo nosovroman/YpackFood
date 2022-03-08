e0fe202358eb9e8c5975130ede2bef0e2aaf42a4
Nosov Roman <roma2000rus@gmail.com>
2022-03-08 17:29:35 +0300

    append scaffold, drawer with hardcode items, rewrite code style for content property

--- app/src/main/java/com/example/ypackfood/activities/MainActivity.kt
+++ app/src/main/java/com/example/ypackfood/activities/MainActivity.kt
@@ -24,6 +24,8 @@ class MvvmViewModel : ViewModel() {
         private set
     lateinit var listCategoryState: LazyListState
         private set
+    lateinit var scaffoldState: ScaffoldState
+        private set
 
     fun listContentStateInit(newListState: LazyListState) {
         listContentState = newListState
@@ -31,6 +33,10 @@ class MvvmViewModel : ViewModel() {
     fun listCategoryStateInit(newListState: LazyListState) {
         listCategoryState = newListState
     }
+    fun scaffoldStateInit(newScaffoldState: ScaffoldState) {
+        scaffoldState = newScaffoldState
+    }
+
 }
 
 @Composable
@@ -38,56 +44,98 @@ fun MainScreen() {
     val mvvmViewModel = MvvmViewModel()
     mvvmViewModel.listContentStateInit(rememberLazyListState())
     mvvmViewModel.listCategoryStateInit(rememberLazyListState())
+    mvvmViewModel.scaffoldStateInit(rememberScaffoldState())
 
-    Column {
-        ToolBarComponent()
-        Spacer(modifier = Modifier.height(10.dp))
-        CategoriesRowComponent(mvvmViewModel)
-        Spacer(modifier = Modifier.height(10.dp))
-        ContentListComponent(mvvmViewModel)
-    }
+    Scaffold(
+        scaffoldState = mvvmViewModel.scaffoldState,
+        drawerContent = { Drawer() },
+        topBar = { ToolBarComponent(mvvmViewModel) },
+        content = {
+            Column {
+                Spacer(modifier = Modifier.height(10.dp))
+                CategoriesRowComponent(mvvmViewModel)
+                Spacer(modifier = Modifier.height(10.dp))
+                ContentListComponent(mvvmViewModel)
+            }
+        }
+    )
 }
 
 @Composable
-fun ToolBarComponent() {
+fun Drawer() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, top = 48.dp),
+        content = {
+            Text("Пункт меню 1", fontSize = 28.sp)
+            Text("Пункт меню 2", fontSize = 28.sp)
+            Text("Пункт меню 3", fontSize = 28.sp)
+        }
+    )
+}
+
+@Composable
+fun ToolBarComponent(mvvmViewModel: MvvmViewModel) {
+    val scope = rememberCoroutineScope()
+
     TopAppBar {
-        IconButton(onClick = { }) { Icon(Icons.Filled.Menu, contentDescription = "Меню") }
+        IconButton(
+            onClick = {
+                scope.launch {
+                    mvvmViewModel.scaffoldState.drawerState.open()
+                }
+            },
+            content = {
+                Icon(Icons.Filled.Menu, contentDescription = "Меню")
+            }
+        )
+
         Spacer(Modifier.weight(1f, true))
         Text("Упак.Еда", fontSize = 22.sp)
         Spacer(Modifier.weight(1f, true))
-        IconButton(onClick = { }) { Icon(Icons.Outlined.ShoppingCart, contentDescription = "Корзина", Modifier.scale(-1.0f, 1.0f) ) }
+
+        IconButton(
+            onClick = { },
+            content = {
+                Icon(Icons.Outlined.ShoppingCart, contentDescription = "Корзина", Modifier.scale(-1.0f, 1.0f) )
+            }
+        )
     }
 }
 
 @Composable
 fun CategoriesRowComponent(mvvmViewModel: MvvmViewModel) {
     val limits = listOf(Pair(0,2), Pair(3,5), Pair(6,8), Pair(9,11), Pair(12,14))
-    val coroutineScope2 = rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
     val chosenCategoryIndex = limits.indexOf(limits.find { mvvmViewModel.listContentState.firstVisibleItemIndex in it.first..it.second })
-    //Log.d("|||her", chosenCategory.toString())
+
     LaunchedEffect(chosenCategoryIndex) {
-        coroutineScope2.launch {
+        scope.launch {
             mvvmViewModel.listCategoryState.animateScrollToItem(chosenCategoryIndex)
         }
     }
 
-    LazyRow(state = mvvmViewModel.listCategoryState, modifier = Modifier.padding(start = 10.dp, end = 10.dp))
-    {
-        itemsIndexed(getAllCategories()) { index, item ->
-            val isChosen = index == chosenCategoryIndex
-            Spacer(modifier = Modifier.padding(start = 5.dp))
-            CategoryComponent(mvvmViewModel = mvvmViewModel, categoryName = item, positionInContent = limits[index].first, isChosen = isChosen)
+    LazyRow(
+        state = mvvmViewModel.listCategoryState,
+        modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+        content = {
+            itemsIndexed(getAllCategories()) { index, item ->
+                val isChosen = index == chosenCategoryIndex
+                Spacer(modifier = Modifier.padding(start = 5.dp))
+                CategoryComponent(mvvmViewModel = mvvmViewModel, categoryName = item, positionInContent = limits[index].first, isChosen = isChosen)
+            }
         }
-    }
+    )
 }
 
 @Composable
 fun CategoryComponent(mvvmViewModel: MvvmViewModel, categoryName: MainCategory, positionInContent: Int, isChosen: Boolean) {
-    val coroutineScope = rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
 
     Button(
         onClick = {
-            coroutineScope.launch {
+            scope.launch {
                 mvvmViewModel.listContentState.animateScrollToItem(positionInContent)
                 Log.d("HelloBtn---", positionInContent.toString())
             }
@@ -97,40 +145,48 @@ fun CategoryComponent(mvvmViewModel: MvvmViewModel, categoryName: MainCategory, 
                 //Log.d("her $currentIndex: ", chosenCategory.toString())
                 MaterialTheme.colors.primary
             } else MaterialTheme.colors.background
-        )
-    ) {
-        Text(text = categoryName.categoryName)
-    }
+        ),
+        content = {
+            Text(text = categoryName.categoryName)
+        }
+    )
 }
 
 @Composable
 fun ContentListComponent(mvvmViewModel: MvvmViewModel) {
-    LazyColumn (state = mvvmViewModel.listContentState, modifier = Modifier.padding(start = 10.dp, end = 10.dp)) {
-        itemsIndexed(mergedList) { index, item ->
-            ContentCardComponent(cardName = item)
-            if (index < mergedList.size - 1) {
-                Spacer(modifier = Modifier.padding(start = 5.dp))
-                Divider(color = MaterialTheme.colors.onBackground, thickness = 1.dp)
+    LazyColumn (
+        state = mvvmViewModel.listContentState,
+        modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+        content = {
+            itemsIndexed(mergedList) { index, item ->
+                ContentCardComponent(cardName = item)
+                if (index < mergedList.size - 1) {
+                    Spacer(modifier = Modifier.padding(start = 5.dp))
+                    Divider(color = MaterialTheme.colors.onBackground, thickness = 1.dp)
+                }
             }
         }
-    }
+    )
 }
 
 @Composable
 fun ContentCardComponent(cardName: String) {
-    Row(modifier = Modifier
-        .padding(top = 8.dp)
-        .fillMaxWidth()
-    ) {
-        Box(
-            modifier = Modifier
-                .width(120.dp)
-                .height(170.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = cardName, color = MaterialTheme.colors.onBackground)
+    Row(
+        modifier = Modifier
+            .padding(top = 8.dp)
+            .fillMaxWidth(),
+        content = {
+            Box(
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(170.dp),
+                contentAlignment = Alignment.Center,
+                content = {
+                    Text(text = cardName, color = MaterialTheme.colors.onBackground)
+                }
+            )
         }
-    }
+    )
 }
 
 
